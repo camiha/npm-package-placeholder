9b8ff9c031456ccbc028f61a665e4bab59fc7b1b
KAMIHARA Takumi <hello.camiha@gmail.com>
2025-01-28 23:26:19 +0900

    feat: initial commit

---
+++ release.config.js
@@ -0,0 +1,18 @@
+export default {
+	plugins: [
+		["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+		[
+			"@semantic-release/release-notes-generator",
+			{ preset: "conventionalcommits" },
+		],
+		"@semantic-release/changelog",
+		"@semantic-release/npm",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+			},
+		],
+		"@semantic-release/github",
+	],
+};
---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		globals: true,
+		coverage: {
+			provider: "v8",
+			exclude: [
+				"dist/**",
+				"vitest.config.ts",
+				"example/**",
+			],
+		},
+	},
+});
