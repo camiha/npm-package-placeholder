97848d816599d4b240a45a1f2e7470fc539d27c3
KAMIHARA Takumi <hello.camiha@gmail.com>
2025-01-29 00:42:35 +0900

    feat!: remove unused function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { echo, hello } from ".";
-
-describe("hello", () => {
-	it("should return 'Hello, World!'", () => {
-		expect(hello()).toBe("Hello, World!");
-	});
-});
+import { echo } from ".";
 
 describe("echo", () => {
 	it("should return the message", () => {
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export const echo = (message: string): string => {
   return message;
 }
-
-export const hello = (): string => {
-  return 'Hello, World!';
-}
